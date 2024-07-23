504008f12e9dd3b304581dac792d8bae20224411
Amaan Sheikh <100550299+amaan784@users.noreply.github.com>
2024-07-23 18:14:17 -0400

    Update Python Starter with env.py

--- Python Starter with env.py
+++ Python Starter with env.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 
 def main():
+    # NOTE: use `pip install python-dotenv` to install the library
+
     load_dotenv()  # Load environment variables from .env file
     file_path = os.environ.get("FILE_PATH")
     
